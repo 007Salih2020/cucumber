a1c372aba437789f3d1f808f468c381c11d85d36
007Salih2020 <66966438+007Salih2020@users.noreply.github.com>
2021-07-19 17:03:00 +0200

    Update SmokeRunner.java
    annotations added

--- src/test/java/com/mycompany/app/extra/SmokeRunner.java
+++ src/test/java/com/mycompany/app/extra/SmokeRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		plugin = {"pretty","json:target/site/results.json"},
-		tags = {"@smoke"},
+		tags = {"@smoke", @Hello, @SPEL-1},
 		features = { "src/test/resources/com/mycompany/app/" },
 		glue = { "com.mycompany.app"}
 		)
