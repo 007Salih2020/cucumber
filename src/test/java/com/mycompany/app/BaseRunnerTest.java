9073d5d15132c296ddaa56c2153913390652565d
007Salih2020 <66966438+007Salih2020@users.noreply.github.com>
2021-07-21 16:35:52 +0200

    Update BaseRunnerTest.java

--- src/test/java/com/mycompany/app/BaseRunnerTest.java
+++ src/test/java/com/mycompany/app/BaseRunnerTest.java
@@ -5,10 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-                plugin ={"json:data/cucumber.json"}, 
-              
-)
+@CucumberOptions( plugin ={"json:data/cucumber.json"} )
 
 
 public class BaseRunnerTest {
