f1bd3d1603c2e6c8f473cb2dcd909df784932eb0
007Salih2020 <S_ozkan@web.de>
2021-07-22 13:05:59 +0200

    some changes

--- src/test/java/com/mycompany/app/extra/SmokeRunner.java
+++ src/test/java/com/mycompany/app/extra/SmokeRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		plugin = {"pretty","json:target/site/results.json"},
-		tags = {"@regression"},
+		tags = {},
 		features = { "src/test/resources/com/mycompany/app/" },
 		glue = { "com.mycompany.app"}
 		)
