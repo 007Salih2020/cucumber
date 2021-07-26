2286b8ab4b22a8f39c1195dafe0f1f2da52fa965
007Salih2020 <S_ozkan@web.de>
2021-07-26 13:06:00 +0200

    some changes

--- src/test/java/com/mycompany/app/BaseRunnerTest.java
+++ src/test/java/com/mycompany/app/BaseRunnerTest.java
@@ -5,11 +5,12 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions( plugin ={"json:target/cucumber.json"}
-        // features= {"src/test/resources/com/mycompany/app/"},
-        // glue = {"com.mycompany.app.steps"}
+@CucumberOptions(
+        strict = true,
+        plugin ={"json:target/cucumber.json"},
+        features= {"src/test/resources/com/mycompany/app/"},
+        glue = { "com.mycompany.app.steps" }
             )
-
 public class BaseRunnerTest {
 
 }
